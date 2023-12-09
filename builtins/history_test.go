ce8a1c50a6b698c2e122dbea37e9d40d90ecbe31
Dang Nguyen <xuanhaidang.nguyen@gmail.com>
2023-12-09 01:03:19 -0600

    u

--- builtins/history_test.go
+++ builtins/history_test.go
@@ -1,35 +1,113 @@
+// package builtins_test
+
+// import (
+// 	"github.com/dangxnguyen/CSCE4600/builtins"
+// 	"io/ioutil"
+// 	"os"
+// 	"strings"
+// 	"testing"
+// )
+
+// func TestSaveToHistoryAndShowHistory(t *testing.T) {
+// 	// Create a temporary directory and change to it
+// 	tmpDir, err := ioutil.TempDir("", "testhistory")
+// 	if err != nil {
+// 		t.Fatalf("Failed to create temporary directory: %v", err)
+// 	}
+// 	defer os.RemoveAll(tmpDir) // Clean up
+
+// 	originalDir, err := os.Getwd()
+// 	if err != nil {
+// 		t.Fatalf("Failed to get current working directory: %v", err)
+// 	}
+
+// 	if err := os.Chdir(tmpDir); err != nil {
+// 		t.Fatalf("Failed to change to temporary directory: %v", err)
+// 	}
+// 	defer os.Chdir(originalDir) // Change back to the original directory
+
+// 	// Test data
+// 	commands := []string{"echo foo", "cd /tmp", "ls -l"}
+
+// 	// Write commands to history
+// 	for _, cmd := range commands {
+// 		if err := builtins.SaveToHistory(cmd); err != nil {
+// 			t.Errorf("SaveToHistory() failed for command '%s': %v", cmd, err)
+// 		}
+// 	}
+
+// 	// Capture output of ShowHistory
+// 	rescueStdout := os.Stdout
+// 	r, w, _ := os.Pipe()
+// 	os.Stdout = w
+
+// 	if err := builtins.ShowHistory(); err != nil {
+// 		t.Errorf("ShowHistory() failed: %v", err)
+// 	}
+
+// 	w.Close()
+// 	out, _ := ioutil.ReadAll(r)
+// 	os.Stdout = rescueStdout
+
+// 	output := string(out)
+// 	// Check if all commands are in the output
+// 	for i, cmd := range commands {
+// 		expected := strings.TrimSpace(cmd) // Trimming spaces for safe comparison
+// 		if !strings.Contains(output, expected) {
+// 			t.Errorf("Expected command '%s' at line %d not found in history output", expected, i+1)
+// 		}
+// 	}
+// }
+
 package builtins_test
 
 import (
 	"github.com/dangxnguyen/CSCE4600/builtins"
-	"io/ioutil"
 	"os"
-	"strings"
 	"testing"
+	"strings"
+	"io/ioutil"
 )
 
-func TestSaveToHistoryAndShowHistory(t *testing.T) {
+func TestSaveToHistory(t *testing.T) {
 	// Create a temporary directory and change to it
-	tmpDir, err := ioutil.TempDir("", "testhistory")
+	tmpDir, err := os.MkdirTemp("", "testhistory")
 	if err != nil {
 		t.Fatalf("Failed to create temporary directory: %v", err)
 	}
 	defer os.RemoveAll(tmpDir) // Clean up
 
-	originalDir, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("Failed to get current working directory: %v", err)
-	}
-
 	if err := os.Chdir(tmpDir); err != nil {
 		t.Fatalf("Failed to change to temporary directory: %v", err)
 	}
-	defer os.Chdir(originalDir) // Change back to the original directory
 
 	// Test data
 	commands := []string{"echo foo", "cd /tmp", "ls -l"}
 
 	// Write commands to history
+	for _, cmd := range commands {
+		if err := builtins.SaveToHistory(cmd); err != nil {
+			t.Errorf("SaveToHistory() failed for command '%s': %v", cmd, err)
+		}
+	}
+
+	// Add additional checks or logic specific to testing SaveToHistory
+}
+
+func TestShowHistory(t *testing.T) {
+	// Create a temporary directory and change to it
+	tmpDir, err := os.MkdirTemp("", "testhistory")
+	if err != nil {
+		t.Fatalf("Failed to create temporary directory: %v", err)
+	}
+	defer os.RemoveAll(tmpDir) // Clean up
+
+	if err := os.Chdir(tmpDir); err != nil {
+		t.Fatalf("Failed to change to temporary directory: %v", err)
+	}
+
+	// Test data
+	commands := []string{"echo foo", "cd /tmp", "ls -l"}
 	for _, cmd := range commands {
 		if err := builtins.SaveToHistory(cmd); err != nil {
 			t.Errorf("SaveToHistory() failed for command '%s': %v", cmd, err)
@@ -50,13 +128,13 @@ func TestSaveToHistoryAndShowHistory(t *testing.T) {
 	os.Stdout = rescueStdout
 
 	output := string(out)
+
 	// Check if all commands are in the output
 	for i, cmd := range commands {
-		expected := strings.TrimSpace(cmd) // Trimming spaces for safe comparison
+		expected := strings.TrimSpace(cmd)
 		if !strings.Contains(output, expected) {
 			t.Errorf("Expected command '%s' at line %d not found in history output", expected, i+1)
 		}
 	}
 }
-
 
